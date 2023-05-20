df25acd87b898f43f4afa160f626f7378e5dfd9e
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 14:35:19 +0330

    user modal created

---
+++ HW4/NewUserModal.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    public class NewUserModal
+    {
+        public string name { get; set; }
+        public long phoneNumber { get; set; }
+        public DateAndTime birthDay { get; set; }
+    }
+}
