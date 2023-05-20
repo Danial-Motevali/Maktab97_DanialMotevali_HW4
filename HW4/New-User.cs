34b1fb9e04f41c2d59d69bfbfc387131d5dd06ea
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 20:12:06 +0330

    path for datafile created

--- HW4/New-User.cs
+++ HW4/New-User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HW4
 {
@@ -10,8 +11,10 @@ namespace HW4
     {
         string? path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName + "\\FileDataStorage.csv";
         string date = string.Empty;
+        
         public void AddingUser(string name , long phoneNumber, DateTime birthDay)
         {
+            FileStream datafile = new FileStream(path);
             date = birthDay.ToString("dd/MM/yyyy");
             
         }
