9845cd98b91d0ee5430a2702d3485d3c4d1a61b2
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 20:20:21 +0330

    fix the birthDay datatype in NewUserModal

--- HW4/NewUserModal.cs
+++ HW4/NewUserModal.cs
@@ -11,6 +11,6 @@ namespace HW4
     {
         public string name { get; set; }
         public long phoneNumber { get; set; }
-        public DateAndTime birthDay { get; set; }
+        public DateTime birthDay { get; set; }
     }
 }
