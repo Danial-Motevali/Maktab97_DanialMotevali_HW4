b90645f23f030834cbf2b9391e99ffda6e199070
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 20:03:11 +0330

    fix the all input from user and create a path for data

--- HW4/New-User.cs
+++ HW4/New-User.cs
@@ -8,9 +8,12 @@ namespace HW4
 {
     public class New_User
     {
+        string? path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName + "\\FileDataStorage.csv";
+        string date = string.Empty;
         public void AddingUser(string name , long phoneNumber, DateTime birthDay)
         {
-
+            date = birthDay.ToString("dd/MM/yyyy");
+            
         }
     }
 }
