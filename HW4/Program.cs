21a4d4924dfca8843770be936ec973c6ae178e35
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 14:11:50 +0330

    created the menu

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int input = 0;
+            do 
+            {
+                Console.WriteLine("--1.New user/2.Show list/3.Updata user/4.Delete user--");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if(input == 1)
+                {
+
+                }else if(input == 2)
+                {
+
+                }else if (input == 3)
+                {
+
+                }else if (input == 4)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Not invalid make sure you pick from menu with number");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (true);
         }
     }
 }
