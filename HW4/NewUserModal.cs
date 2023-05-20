583ecd4051145693f1b1701cc2f8d3f8bfd932de
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 22:51:06 +0330

    add time of creating the new user to NewUserModal

--- HW4/NewUserModal.cs
+++ HW4/NewUserModal.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace HW4
         public string name { get; set; }
         public long phoneNumber { get; set; }
         public string birthDay { get; set; }
+        public string time { get; set; }
         
     }
 }
