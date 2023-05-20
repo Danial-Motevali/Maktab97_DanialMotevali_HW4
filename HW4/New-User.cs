6cf55596658c9bd30ed9dd8789fe1706a1dfd11e
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-20 20:35:36 +0330

    writing the data to file

--- HW4/New-User.cs
+++ HW4/New-User.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 using System.IO;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace HW4
 {
     public class New_User
     {
-        string? path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName + "\\FileDataStorage.csv";
+        string? path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName + "\\FileDataStorage.txt";
         string date = string.Empty;
         
         public void AddingUser(string name , long phoneNumber, DateTime birthDay)
         {
-            FileStream datafile = new FileStream(path);
             date = birthDay.ToString("dd/MM/yyyy");
             
+            NewUserModal model1 = new NewUserModal();
+            model1.name = name;
+            model1.phoneNumber = phoneNumber;
+            model1.birthDay = date;
+
+            var addtofile = JsonConvert.SerializeObject(model1);
+            StreamWriter dataFile = new StreamWriter(path);
+
+            dataFile.Write(addtofile);
+            dataFile.Close();
         }
     }
 }
