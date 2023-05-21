30ba56fd56e67ec0704b2601f17724473573e28e
Danial Motevali <danial.motevali.82@gmail.com>
2023-05-21 15:43:58 +0330

    sorting the file data by id number Asceding

--- HW4/ShowTheList.cs
+++ HW4/ShowTheList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace HW4
 {
-    internal class ShowTheList
+    public class ShowTheList
     {
+        object[] list;
+        int id = 1;
+        public object[] ShowList(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            
+            foreach (string line in lines)
+            {
+                var speficLine = JsonConvert.DeserializeObject<NewUserModal>(line);
+                if( speficLine.id == id)
+                {
+                    list.Append(speficLine);
+                    this.id++;
+                }
+            }
+
+            return list;
+        }
     }
 }
